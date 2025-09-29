994c38ad2a2a7a967783b33932e007a1e52a1e56
AchoLito <javier.martinmulero@gmail.com>
2025-09-29 20:47:18 +0200

    Lectura de movimiento

--- _Project/Assets/2_Scripts/Player/IPlayerController.cs
+++ _Project/Assets/2_Scripts/Player/IPlayerController.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewEmptyCSharpScript
+namespace PlayerComponents
 {
-    
+    public interface IPlayerController
+    {
+        void Atack();
+        void Move(Vector2 movement);
+    }
 }
--- _Project/Assets/2_Scripts/Player/PlayerController.cs
+++ _Project/Assets/2_Scripts/Player/PlayerController.cs
@@ -1,16 +1,66 @@
+using PlayerComponents;
+using Unity.Plastic.Newtonsoft.Json.Linq;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace PlayerComponents
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerController : MonoBehaviour, IPlayerController
     {
-        
-    }
+        [SerializeField] private float speed = 5f;
+        [SerializeField] private float damage = 0f;
+        private Vector2 movement = new Vector2();
+        private bool isMoving = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private InputAction moveCommand;
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*
+            isMoving = moveCommand.triggered;
+            Debug.Log("ACCION: " + moveCommand.triggered);
+            
+            */
+
+            if (isMoving)
+            {
+                Move(movement);
+            }
+        }
+
+        /// <Atack>
+        /// Método de ataque del jugador
+        /// </Atack>
+        public void Atack()
+        {
+            Debug.Log("PICAAAAAAASO");
+        }
+
+        /// <Movement>
+        /// Método de movimiento del jugador
+        /// Usa la posición de la cámara para moverse
+        /// </Movement>
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Vector2 valor = context.ReadValue<Vector2>();
+
+            if (valor == Vector2.zero)
+            {
+                isMoving = false;
+                movement = Vector2.zero;
+                return;
+            }
+
+            isMoving = true;
+            movement = valor;
+        }
+
+        public void Move(Vector2 movement)
+        {
+            Debug.Log("CAMINAAAAAAANDO: " + movement);
+        }
     }
 }
