7af43bf77ed997aa45b6dac2970bff1934e55172
UnaiPas04 <unaipastranatorres@gmail.com>
2025-10-02 19:42:02 +0200

    Main UI Navigation

--- _Project/Assets/2_Scripts/UI Scripts/MainMenu/MainMenuUI.cs
+++ _Project/Assets/2_Scripts/UI Scripts/MainMenu/MainMenuUI.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region SERIALIZABLE
+    [SerializeField] GameObject go_mainWindow;
+    [SerializeField] GameObject go_settingsWindow;
+    [SerializeField] GameObject go_creditsWindow;
+    [SerializeField] GameObject go_closeIcon;
+    #endregion
+    #region PUBLIC VARS
+    public enum Windows
     {
-        
+        Main,
+        Settings,
+        Credits
     }
+    #endregion
+    #region PRIVATE FUNCS
+    private void Awake()
+    {
+        SwitchWindow(Windows.Main);
+    }
+    void SwitchWindow(Windows nextWindow)
+    {
+        go_mainWindow.SetActive(nextWindow == Windows.Main);
+        go_settingsWindow.SetActive(nextWindow == Windows.Settings);
+        go_creditsWindow.SetActive(nextWindow == Windows.Credits);
+        go_closeIcon.SetActive(nextWindow != Windows.Main);
+    }
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #region PUBLIC FUNCS
+    public void SwitchToMain() => SwitchWindow(Windows.Main);
+    public void SwitchToSettings() => SwitchWindow(Windows.Settings);
+    public void SwitchToCredits() => SwitchWindow(Windows.Credits);
+    public void SwitchToGameScene() => SceneManager.LoadScene("2_GAME_SCENE");
+    #endregion
 }
