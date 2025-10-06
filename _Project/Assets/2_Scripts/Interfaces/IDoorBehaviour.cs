874bef473819371fdd8120582e4d661d8e2d9037
AchoLito <javier.martinmulero@gmail.com>
2025-10-06 18:35:37 +0200

    Interfaces

---
+++ _Project/Assets/2_Scripts/Interfaces/IDoorBehaviour.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IDoorBehaviour
+{
+    void EnterToRoom();
+}
---
+++ _Project/Assets/2_Scripts/Interfaces/IDoorsManagement.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IDoorsManagement
+{
+    void ChooseRoom();
+}
