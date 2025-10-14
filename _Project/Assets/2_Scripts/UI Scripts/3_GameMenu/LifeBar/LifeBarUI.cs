d2c5b691fd2dbc15e51f2bd96fd98ff7b8a87416
UnaiPas04 <unaipastranatorres@gmail.com>
2025-10-14 21:03:42 +0200

    Life Bar UI updated

--- _Project/Assets/2_Scripts/UI Scripts/3_GameMenu/LifeBar/LifeBarUI.cs
+++ _Project/Assets/2_Scripts/UI Scripts/3_GameMenu/LifeBar/LifeBarUI.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class LifeBarUI : MonoBehaviour
 {
     [SerializeField] DamageableComponent damageableComponent;
     [SerializeField] RectTransform rectT_lifeBarSlider;
+    [SerializeField] Image img_lifeBarSlider;
     int MaxLife = 0;
     private void OnEnable()
     {
@@ -15,7 +16,7 @@ public class LifeBarUI : MonoBehaviour
     {
         //Right es 100 cuando value es 0, y 0 cuando value es 100
         float percent = 100.0f * value / MaxLife;//division decimal, no entera
-        int right = 100 - (int)percent;
-        rectT_lifeBarSlider.offsetMax = new Vector2(-right,0); // Right y Top
+        float normalicedVal = percent / 100;
+        img_lifeBarSlider.fillAmount = normalicedVal;
     }
 }
