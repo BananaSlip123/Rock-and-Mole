3403ccb54753e2647912cfaabb6d78a50ca1a054
AchoLito <javier.martinmulero@gmail.com>
2025-11-04 18:39:48 +0100

    Clase campamento

---
+++ _Project/Assets/2_Scripts/Mine Run Scripts/CampfireHealing.cs
@@ -0,0 +1,17 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CampfireHealing : MonoBehaviour
+{
+    [SerializeField] const int healing = 20;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
