2bf9df3c913dc242f90fd9e049c353bbf5dde37e
AchoLito <javier.martinmulero@gmail.com>
2025-11-06 18:39:13 +0100

    Build hecha

--- _Project/Assets/2_Scripts/Interfaces/IPlayerStats.cs
+++ _Project/Assets/2_Scripts/Interfaces/IPlayerStats.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public interface IPlayerStats
 {
-    void ChangeClothes();
+    void ChangeClothes(ClothStatsScripteableObject newCloth);
     void ChangePickaxe(PickaxeStatsScripteableObject newPickaxe);
     public void ResetStats();
+
+    void ChangeSomething(PickaxeStatsScripteableObject newPickaxe, ClothStatsScripteableObject newCloth);
 }
