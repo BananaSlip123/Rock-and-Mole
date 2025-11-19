2784621b392469a3a05571cb0d8610010e15a7d2
UnaiPas04 <unaipastranatorres@gmail.com>
2025-11-19 19:40:02 +0100

    Materiales partida con iconos

--- _Project/Assets/2_Scripts/UI Scripts/3_GameMenu/MaterialsCollectedUI/MaterialsCollectedUI.cs
+++ _Project/Assets/2_Scripts/UI Scripts/3_GameMenu/MaterialsCollectedUI/MaterialsCollectedUI.cs
@@ -5,15 +5,32 @@ using System.Collections.Generic;
 using System.Collections;
 public class MaterialsCollectedUI : MonoBehaviour
 {
-    [SerializeField] GameObject[] gos_callOuts;
+    #region SERIALIZABLE FIELDS
+    [SerializeField] CallOutElements[] callOuts;
 
     TextMeshProUGUI[] txts_callOutsTexts;
     Image[] imgs_callOutsImages;
+    Image[] imgs_callOutsIcons;
+
+    [System.Serializable]
+    public struct CallOutElements
+    {
+        public TextMeshProUGUI txt_callOutText;
+        public Image img_callOutImage;
+        public Image img_callOutIcon;
+    }
+    #endregion
+
+
     int size;
     bool isInit = false;
 
     List<Message> messages = new List<Message>();
 
+    public static Dictionary<MaterialName, MaterialsData.IconData> Icons
+    {
+        get => MaterialsData.Icons;
+    }
     private void OnEnable()
     {
         Init();
@@ -21,6 +38,25 @@ public class MaterialsCollectedUI : MonoBehaviour
         GameData.RunInventory.OnMaterialsEarned += OnMaterialAdd;
         StartCoroutine(ActualizeUI());
         
+    }
+    void Init()
+    {
+        if (isInit) return;
+
+        size = callOuts.Length;
+        txts_callOutsTexts = new TextMeshProUGUI[size];
+        imgs_callOutsImages = new Image[size];
+        imgs_callOutsIcons = new Image[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            txts_callOutsTexts[i] = callOuts[i].txt_callOutText;
+            imgs_callOutsIcons[i] = callOuts[i].img_callOutIcon;
+            imgs_callOutsImages[i] = callOuts[i].img_callOutImage;
+        }
+        callOuts = null;
+
+        isInit = true;
     }
     private void OnDisable()
     {
@@ -43,10 +79,22 @@ public class MaterialsCollectedUI : MonoBehaviour
             int idx = 0;
             for (int i = messages.Count - 1; i >= 0; i--)
             {
-                txts_callOutsTexts[idx].text = messages[i].GetMessage();
                 float alpha = messages[i].GetAlpha();
+
+                //Texto
+                txts_callOutsTexts[idx].text = messages[i].GetAmountToString();
                 txts_callOutsTexts[idx].alpha = alpha;
+
+                //Fondo
                 imgs_callOutsImages[idx].color = new Color(0,0,0,alpha*0.1f);
+
+                //Icono
+                MaterialsData.IconData iconData = Icons[messages[i].GetMaterialName()];
+
+                imgs_callOutsIcons[idx].sprite = iconData.Sprite;
+                Color materialColor = iconData.Color;
+                materialColor.a = alpha;
+                imgs_callOutsIcons[idx].color = materialColor;
 
                 idx++;
                 if (idx >= size) break;
@@ -55,27 +103,13 @@ public class MaterialsCollectedUI : MonoBehaviour
             {
                 txts_callOutsTexts[idx].alpha = 0;
                 imgs_callOutsImages[idx].color = new Color(0,0,0,0);
+                imgs_callOutsIcons[idx].color = new Color(0,0,0,0);
             }
 
             yield return new WaitForSeconds(deltaTime);
         }
     }
-    void Init()
-    {
-        if (isInit) return;
-
-        size = gos_callOuts.Length;
-        txts_callOutsTexts = new TextMeshProUGUI[size];
-        imgs_callOutsImages = new Image[size];
-
-        for (int i = 0; i < size; i++)
-        {
-            txts_callOutsTexts[i] = gos_callOuts[i].GetComponentInChildren<TextMeshProUGUI>();
-            imgs_callOutsImages[i] = gos_callOuts[i].GetComponent<Image>();
-        }
-
-        isInit = true;
-    }
+    
 
     class Message
     {
@@ -90,10 +124,9 @@ public class MaterialsCollectedUI : MonoBehaviour
             _amount = amount;
             _lifeTime = 0;
         }
-        public string GetMessage()
-        {
-            return $"+{_amount} de {GameData.MaterialName2String(_name)}";
-        }
+        public string GetAmountToString() => $"x{_amount}";
+        public MaterialName GetMaterialName() => _name;
+
         public bool Actualize(float deltaTime)
         {
             _lifeTime += deltaTime;
