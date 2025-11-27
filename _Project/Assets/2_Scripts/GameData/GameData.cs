774b96c1c637476694ca7aea56ca3af7074332a2
AchoLito <javier.martinmulero@gmail.com>
2025-11-27 19:42:13 +0100

    Tablones en las puertas

--- _Project/Assets/2_Scripts/GameData/GameData.cs
+++ _Project/Assets/2_Scripts/GameData/GameData.cs
@@ -49,7 +49,7 @@ public static class GameData
     {
         { EnemyName.Mouse,new List<MaterialName>() { MaterialName.Ambar, MaterialName.Hierro, MaterialName.Bronce, MaterialName.Carbon, MaterialName.Cuarzo, MaterialName.RolloTela } },
         { EnemyName.Bunny, new List<MaterialName>() { MaterialName.Ambar, MaterialName.Hierro, MaterialName.Bronce, MaterialName.Carbon, MaterialName.Rubi, MaterialName.Obsidiana }},
-        { EnemyName.Golem, new List<MaterialName>() { MaterialName.Ambar, MaterialName.Hierro, MaterialName.Carbon, MaterialName.Esmeralda, MaterialName.Bronce, } },
+        { EnemyName.Golem, new List<MaterialName>() { MaterialName.Ambar, MaterialName.Hierro, MaterialName.Carbon, MaterialName.Esmeralda, MaterialName.Bronce } },
         { EnemyName.GolemBoss,new List<MaterialName>() { MaterialName.Rubi, MaterialName.Esmeralda, MaterialName.Hierro, MaterialName.Cuarzo, MaterialName.Obsidiana, MaterialName.Bronce } }
     };
 
@@ -114,7 +114,12 @@ public static class GameData
 
     static MaterialName EnemyMaterial(EnemyName type, MaterialRarity rarity)
     {
-        List<MaterialName> sortedMaterials = SortedMaterialsByRarityAndEnemy(rarity, type);
+        List<MaterialName> sortedMaterials;
+        do
+        {
+            sortedMaterials = SortedMaterialsByRarityAndEnemy(rarity, type);
+            Debug.Log(sortedMaterials.Count);
+        } while (sortedMaterials.Count == 0);
         return sortedMaterials[UnityEngine.Random.Range(0, sortedMaterials.Count)];
     }
 
