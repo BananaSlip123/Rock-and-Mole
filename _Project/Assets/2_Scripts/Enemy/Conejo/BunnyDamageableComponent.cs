e0ec9b72a040cca1284a24764504b41170f0bc3b
AchoLito <javier.martinmulero@gmail.com>
2025-12-09 17:33:39 +0100

    Ajuste vida enemigos

--- _Project/Assets/2_Scripts/Enemy/Conejo/BunnyDamageableComponent.cs
+++ _Project/Assets/2_Scripts/Enemy/Conejo/BunnyDamageableComponent.cs
@@ -16,6 +16,8 @@ public class BunnyDamageableComponent : MonoBehaviour, IDamageableComponent
     {
         if (BiomeManager.CurrentBiome == BiomeName.undergroundForest)
             health = (int)(health * 1.5f);
+        else
+            health = (int)(health * 0.8f);
     }
     private void Update()
     {
