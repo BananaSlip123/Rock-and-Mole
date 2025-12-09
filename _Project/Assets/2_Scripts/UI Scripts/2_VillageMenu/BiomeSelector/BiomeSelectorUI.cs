fbf03d3aee3bdb09c50a49981194662245b57f2f
AchoLito <javier.martinmulero@gmail.com>
2025-12-09 16:24:50 +0100

    Cambio espuma

--- _Project/Assets/2_Scripts/UI Scripts/2_VillageMenu/BiomeSelector/BiomeSelectorUI.cs
+++ _Project/Assets/2_Scripts/UI Scripts/2_VillageMenu/BiomeSelector/BiomeSelectorUI.cs
@@ -80,10 +80,10 @@ public class BiomeSelectorUI : MonoBehaviour
 
         AudioManager.Instance.PlayMusic(AudioManager.MusicType.EnemyFightMusic);
         BiomeManager.CurrentBiome = SelectedBiome;
-        SceneManager.LoadScene("2_CombatRoom");
+        //SceneManager.LoadScene("2_CombatRoom");
         //SceneManager.LoadScene("5_RescueRoom");
 
-        //SceneManager.LoadScene("6_CampamentRoom");
+        SceneManager.LoadScene("6_CampamentRoom");
         //SceneManager.LoadScene("7_DarkRoom");
     }
     #endregion
